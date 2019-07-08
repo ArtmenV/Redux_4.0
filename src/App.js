545ec2acdaae56e1b17c67e9b0030d68ad57c0d0
Artmen_V <viliutich.artem@gmail.com>
2019-07-08 14:29:06 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Counter from "./components/counter";
 import Body from "./components/body";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import {Route} from 'react-router-dom'
+import Contact from "./components/header/link/contact";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Counter />
-      <Body/>
-      <Footer/>
-    </>
+      <Route component={Header}/>
+      <Route path="/counter" component={Counter}/>
+      <Route component={Body}/>
+      <Route component={Footer}/>
+      <Route path="/contact" component={Contact}/>
+      </>
       );
 }
 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './index.css'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="container__header">
       <ul className="container__lineListHeader">
-        <li>contact</li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
         <li>about su</li>
         <li>apportunity</li>
         <li>link social</li>
-        <li>world</li>
+        <li><NavLink to="/counter">Counter</NavLink></li>
       </ul>
     </div>
-
   )
 }
 
